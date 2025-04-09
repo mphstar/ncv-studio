e6751a2a1facb5c76e297415f485ad8c289fb234
mphstar <bintang.simdig20@gmail.com>
2025-04-09 15:12:09 +0700

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ncv-studio.vercel.app',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.7,
+  }
+  
